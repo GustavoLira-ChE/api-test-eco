24a7f3b081451327d8ce6db48ea3bb628af7f768
Gustavo Lira <gu.lirar23@gmail.com>
2023-05-16 13:02:21 -0500

    add: interfaces to contain data information and connection body object

---
+++ src/app/models/client.model.ts
@@ -0,0 +1,11 @@
+export interface Client {
+  strEstatus: string
+  strID: string
+  strNombre: string
+  strContacto: string
+  strEmail: string
+  strUsuario: string
+  strUsuarioNombre: string
+  intDias: number
+  strTipo: string
+}
---
+++ src/app/models/connectionObject.model.ts
@@ -0,0 +1,10 @@
+export interface Connection{
+  strAccion: number
+  strUsuario: string
+  strSucursal: string
+  strPeriodo: number
+  strEjercicio: number
+  strID: string
+  strCoordinador: string
+  strConexion: string
+}
