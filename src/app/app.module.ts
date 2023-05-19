f6e32377cac9fec811265b2f0fa0474ea8da310a
Gustavo Lira <gu.lirar23@gmail.com>
2023-05-19 11:31:30 -0500

    add: FormModule to us ngModel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ClientCardContainerComponent } from './components/client-card-container/client-card-container.component';
@@ -14,7 +15,8 @@ import { ClientCardComponent } from './components/client-card/client-card.compon
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
